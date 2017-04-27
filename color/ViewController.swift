f0cefd5213c386023a4e0fd5a53f0858744a912f
nhatminh <nhatminh291297@gmail.com>
2017-04-27 11:39:14 +0700

    yfgdty

--- color/ViewController.swift
+++ color/ViewController.swift
@@ -8,20 +8,83 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate  {
 
+    let imagePicker = UIImagePickerController()
+    var baseImage = UIImage()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        imagePicker.delegate = self
+        createColorListButton()
+        createAlbumButton()
+        createCameraButton()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func createColorListButton() {
+        let colorlistButton = UIButton()
+        colorlistButton.frame = CGRect(x: self.view.bounds.size.width/4, y: self.view.bounds.size.height/3, width: self.view.bounds.size.width/2, height: 50)
+        colorlistButton.setTitle("Color List", for: .normal)
+        colorlistButton.setTitleColor(UIColor.white, for: .normal)
+        colorlistButton.backgroundColor = UIColor.blue
+        colorlistButton.addTarget(self, action: #selector(pushToColorListView), for: .touchUpInside)
+        colorlistButton.isUserInteractionEnabled = true
+        self.view.addSubview(colorlistButton)
+    }
+    
+    func pushToColorListView() {
+        let newViewController = ColorListTVC()
+        self.navigationController?.pushViewController(newViewController, animated: true)
     }
 
+    func createAlbumButton() {
+        let albumButton = UIButton()
+        albumButton.frame = CGRect(x: self.view.bounds.size.width/4, y: self.view.bounds.size.height/2, width: self.view.bounds.size.width/2, height: 50)
+        albumButton.setTitle("Go to Album", for: .normal)
+        albumButton.setTitleColor(UIColor.white, for: .normal)
+        albumButton.backgroundColor = UIColor.cyan
+        albumButton.addTarget(self, action: #selector(pushToAlbumView), for: .touchUpInside)
+        albumButton.isUserInteractionEnabled = true
+        self.view.addSubview(albumButton)
+    }
+ 
+    func pushToAlbumView() {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
     
     
-
+    func createCameraButton() {
+        let cameraButton = UIButton()
+        cameraButton.frame = CGRect(x: self.view.bounds.size.width/4, y: self.view.bounds.size.height*2/3, width: self.view.bounds.size.width/2, height: 50)
+        cameraButton.setTitle("Go to Camera", for: .normal)
+        cameraButton.setTitleColor(UIColor.white, for: .normal)
+        cameraButton.backgroundColor = UIColor.purple
+        cameraButton.addTarget(self, action: #selector(pushToCameraView), for: .touchUpInside)
+        cameraButton.isUserInteractionEnabled = true
+        self.view.addSubview(cameraButton)
+    }
+    
+    func pushToCameraView() {
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .camera
+        
+        self.present(imagePicker, animated: true, completion: nil)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
+        if let pickedImage = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            baseImage = pickedImage
+        }
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 }
 
